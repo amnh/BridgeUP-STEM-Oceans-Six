5171e3bc754d0af9750a6837f8bcaa90acb93ca5
Katy <katyabbott@berkeley.edu>
2018-11-28 17:27:33 -0500

    test

---
+++ netCDF_script_KA.py
@@ -0,0 +1,8 @@
+from netCDF4 import Dataset #Importing library tool, NECESSARY
+dataset = Dataset("/Users/katyabbott/Documents/GitHub/BridgeUP-STEM-Oceans-Six/datasets/sea_surface_temp.nc") #creating a variable called dataset, where dataset is our dataset we want to use. NECESSARY
+dataset.ncattrs() #tells me about the metadata of my dataset COOL but not NECESSARY
+
+
+
+
+
