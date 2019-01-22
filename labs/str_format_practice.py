015f3da4bf57e4ebf839ac9ceb5876a77789eb60
Katy <katyabbott@berkeley.edu>
2019-01-22 12:05:11 -0500

    Adding labs for internships

---
+++ labs/str_format_practice.py
@@ -0,0 +1,16 @@
+## Question 1
+
+
+
+
+
+## Question 2
+
+
+
+## Question 3
+
+
+
+## Question 4
+dates = [datetime.date(1995, 10, 12), datetime.date(2000, 11, 17), datetime.date(2004, 3, 1), datetime.date(2012, 6, 30)]
