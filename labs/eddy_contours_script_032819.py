d081ae70c22f8037f9758526f3a91140661b7a14
Katy <katyabbott@berkeley.edu>
2019-03-28 12:53:17 -0400

    Adding materials for 3/28/19

---
+++ labs/eddy_contours_script_032819.py
@@ -0,0 +1,38 @@
+## Put any import statements here
+
+
+
+
+
+## Load in our Bermuda SLA dataset here.
+
+
+
+
+
+## Make a contour plot of a single day of sea level anomaly data here. 
+## Make sure to save the contour plot as a variable!
+## Use levels that are spaced evenly every 0.01 meters (this is the units of SLA)
+day = 0
+
+
+
+
+
+## Write a function that takes in a contour plot object and an index and 
+## returns the level value at that index and a list containing all of the Path objects 
+## for that level. (all of the contour lines that share that level value)
+
+
+
+
+## Write code to create a Pandas dataframe where each contour line and 
+## corresponding level is stored as an individual row.
+## There are many ways you can do this. The goal is a dataframe that looks like:
+
+
+
+    
+
+
+ 
